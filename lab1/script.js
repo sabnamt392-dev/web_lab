2dc2722b2f0a22d958980d6d645b5817e869d104
sabnamt392-dev <sabnamt392@gmail.com>
2026-03-29 18:04:12 +0545

    Add files via upload
    portfolio

---
+++ lab1/script.js
@@ -0,0 +1,16 @@
+// script.js
+const observerOptions = {
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, observerOptions);
+
+document.querySelectorAll('.card').forEach(card => {
+    observer.observe(card);
+});
